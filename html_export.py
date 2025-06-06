90c9524c7fb04c8e2440744565118f72107b4dfa
Martin <mcarstensen79@gmail.com>
2025-06-06 20:08:30 +0200

    Update html_export.py

--- html_export.py
+++ html_export.py
@@ -74,7 +74,8 @@ def generate_html(fahrer_name, eintraege, kw, start_date, css_styles):
     return html
 
 
-# CSS-Stile = """
+# CSS-Stile 
+css_styles = """
 body {
   font-family: 'Inter', Arial, sans-serif;
   background: #e3e7ee;
