34334760347aa147eda6b00c7c4d2e6a5a6875d8
Martin <mcarstensen79@gmail.com>
2025-06-06 19:34:54 +0200

    Update html_export.py

--- html_export.py
+++ html_export.py
@@ -45,7 +45,6 @@ if uploaded_file:
             except:
                 continue
 
-            kw = get_kw(datum_dt)
             wochentag_en = datum_dt.strftime("%A")
             wochentag = wochentage_deutsch_map.get(wochentag_en, wochentag_en)
 
@@ -82,12 +81,12 @@ if uploaded_file:
                 continue
 
             start_datum = min(eintraege.keys())
-            start_sonntag = start_datum - pd.Timedelta(days=(start_datum.weekday() + 1) % 7)
-            kw = get_kw(start_sonntag)
+            start_montag = start_datum - pd.Timedelta(days=(start_datum.weekday()))  # Montag als Wochenanfang
+            kw = get_kw(start_montag)
 
             wochen_eintraege = []
             for i in range(7):
-                tag_datum = start_sonntag + pd.Timedelta(days=i)
+                tag_datum = start_montag + pd.Timedelta(days=i)
                 wochentag_en = tag_datum.strftime("%A")
                 wochentag = wochentage_deutsch_map.get(wochentag_en, wochentag_en)
 
