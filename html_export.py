2a721655c15836fd72d219067db578f4daedd4fa
Martin <mcarstensen79@gmail.com>
2025-06-13 20:20:00 +0200

    Update html_export.py

--- html_export.py
+++ html_export.py
@@ -373,9 +373,7 @@ if uploaded_files:
                         filename = f"KW{kw:02d}_{name_clean}.html"
 
                         html_code = generate_html(fahrer_name, wochen_eintraege, kw, start_sonntag, css_styles)
-                        for eintrag in wochen_eintraege:
-                            html_code += f"<li>{eintrag}</li>"
-                        html_code += "</ul></body></html>"
+                        
 
                         folder_name = f"KW{kw:02d}"
                         full_path = os.path.join(tmpdir, folder_name, filename)
